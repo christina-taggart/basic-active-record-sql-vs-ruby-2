844a29f089d23c1c33ec1e51a8dba7d5eb7030a8
Spencer Smitherman & Ricardo A. Rubio <pair+bigspencey.rickarubio@devbootcamp.com>
2014-01-21 16:19:02 -0800

    completed release 1 and 2

--- source/setup.rb
+++ source/setup.rb
@@ -65,3 +65,48 @@ module StudentDB
     )
   end
 end
+
+class Student
+
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @gender = args[:gender]
+    @birthday = args[:birthday]
+    @email = args[:email]
+    @phone = args[:phone]
+  end
+
+  def all
+    $db.execute ("SELECT * FROM students")
+  end
+
+  def where(field, data, operator="=")
+    $db.execute("SELECT * FROM students WHERE #{field} #{operator} '#{data}'")
+  end
+
+  def add
+    $db.execute(
+      <<-SQL
+        INSERT INTO students
+          (first_name, last_name, gender, birthday, email, phone, created_at, updated_at)
+        VALUES
+          ("#{@first_name}","#{@last_name}", "#{@gender}", "#{@birthday}", "#{@email}", "#{@phone}", DATETIME('now'), DATETIME('now'));
+      SQL
+    )
+  end
+
+  def delete(field, data, operator="=")
+    $db.execute("DELETE FROM students WHERE #{field} #{operator} '#{data}'")
+  end
+end
+
+#------------DRIVE CODE--------------#
+
+test_student = Student.new({first_name: 'BRUCE', last_name: 'LEE', gender: 'Female', birthday: '2011-12-31', email: 'spencer.smitherman@gmail.com', phone: '555-333-8888'})
+#test_student.add # Uncomment to add this student into the database
+ p test_student.all
+test_student.where('first_name', 'Brick') #=> Should see the record for student Brick Thornton
+p test_student
+test_student.where('birthday', '2011%', 'LIKE')
+test_student.delete('id', 8)
