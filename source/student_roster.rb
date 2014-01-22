3591b133eee052256cbefd3eeb58a4672a34ba15
Christina Taggart & <pair+christina-taggart.hzla00@devbootcamp.com>
2014-01-21 16:12:56 -0800

    Release 2: Add methods to the Student Class to execute SQL commands

--- source/student_roster.rb
+++ source/student_roster.rb
@@ -1,7 +1,8 @@
-class Student
+require 'sqlite3'
 
+class Student
+  attr_reader :first_name, :last_name, :gender, :birthday, :email, :phone, :created_at, :updated_at, :data
   def initialize(data = {})
-    @id = data[:id]
     @first_name = data[:first_name]
     @last_name = data[:last_name]
     @gender = data[:gender]
@@ -11,6 +12,53 @@ class Student
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
   end
+
+  def add(database)
+    database.execute(
+      <<-SQL
+        INSERT INTO students
+          (first_name, last_name, gender, birthday, email, phone, created_at, updated_at)
+        VALUES
+          ('#{first_name}','#{last_name}', '#{gender}', '#{birthday}', '#{email}', '#{phone}', '#{created_at}', '#{updated_at}');
+        SQL
+    )
+  end
+
+
+  def delete(database)
+    database.execute("delete from students where first_name = '#{first_name}' and last_name = '#{last_name}'")
+  end
+
+  def self.all(database)
+    database.execute("select first_name, last_name from students").each {|student| puts "#{student[0]} #{student[1]}"}
+  end
+
+  def self.first_name(database, first_name)
+    database.execute("select first_name, last_name from students where first_name = '#{first_name}'").each {|student| puts "#{student[0]} #{student[1]}"}
+  end
+
+  def self.attribute(database, attribute, value)
+    database.execute("select first_name, last_name from students where #{attribute} = '#{value}'").each {|student| puts "#{student[0]} #{student[1]}"}
+  end
+
+  def self.birthdays_this_month(database, month)
+    database.execute("select first_name, last_name from students where strftime('%m', birthday) = #{month}").each {|student| puts "#{student[0]} #{student[1]}"}
+  end
+
+  def self.order_by_birthday(database)
+    database.execute("select first_name, last_name from students order by birthday").each {|student| puts "#{student[0]} #{student[1]}"}
+  end
+
 end
 
-bob = Student.new()
+
+bob = Student.new(first_name: 'Bob')
+db = SQLite3::Database.new "students.db"
+
+# bob.add(db)
+bob.delete(db)
+Student.all(db)
+Student.first_name(db, 'Brick')
+Student.attribute(db, :first_name, 'Brick')
+Student.birthdays_this_month(db, 04)
+Student.order_by_birthday(db)
