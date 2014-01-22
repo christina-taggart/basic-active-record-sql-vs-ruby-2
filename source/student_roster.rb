68dfd8f78997dc5d4a24aeb074a99d499f5a5aa2
David Goodman & Eli Shkurkin <pair+dgoodman1224.shkurkin@devbootcamp.com>
2014-01-21 16:07:59 -0800

    Release 2: make it do stuff

--- source/student_roster.rb
+++ source/student_roster.rb
@@ -1,6 +1,7 @@
 require_relative 'setup.rb'
 
 class Student
+  attr_reader :first_name, :last_name, :gender, :birthday, :email, :phone
   def initialize(args)
     @first_name = args[:first_name]
     @last_name = args[:last_name]
@@ -11,26 +12,38 @@ class Student
   end
 
   def save
-    $db.execute("INSERT INTO students (first_name, last_name, ")
+    $db.execute(
+        "INSERT INTO students
+            (first_name, last_name, gender, birthday, email, phone, created_at, updated_at)
+        VALUES
+            (?, ?, ?, ?, ?, ?, DATETIME('now'), DATETIME('now'))",
+        [@first_name, @last_name, @gender, @birthday, @email, @phone]
+    )
   end
 
   def self.delete(where_val)
-
+    $db.execute(
+        "DELETE FROM students
+        WHERE #{where_val}"
+      )
   end
 
   def self.all
+    $db.execute("SELECT * FROM students").each{|student| p student}
   end
 
   def self.where(where_val)
+    $db.execute("SELECT * FROM students WHERE #{where_val}").each{|student| p student}
   end
 
 end
 
 # StudentDB.setup
-StudentDB.seed
+#StudentDB.seed
 
-# vals = {first_name: "David", last_name: "Goodman", gender: "male",
-# birthday: "1990-09-04", email: "david@david.com", phone: "555-555-5555"}
-# new_student = Student.new(vals)
-puts $db.execute("select * from students")
-
+vals = {first_name: "David", last_name: "Goodman", gender: "male",
+birthday: "1990-09-04", email: "david@david.com", phone: "555-555-5555"}
+new_student = Student.new(vals)
+new_student.save
+Student.delete("last_name='Goodman'")
+Student.where("last_name = 'Thornton' ORDER BY first_name")
